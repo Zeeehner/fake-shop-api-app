ff7475e628b41d09888a062bbd07af2ade3ecd9f
Noah Ra <rathaj9@gmail.com>
2025-03-25 13:52:39 +0100

    Ticket 2.1 - 2.4

--- app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/data/model/Auth.kt
+++ app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/data/model/Auth.kt
@@ -1,17 +1,6 @@
 package de.syntax_institut.jetpack.a04_05_online_shopper.data.model
 
-<<<<<<<< HEAD:app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/data/model/Auth.kt
+
 data class Auth(
     val token: String
-========
-data class Product(
-    val id: Int,
-    val title: String,
-    val price: Float,
-    val description: String,
-    val category: String,
-    val image: String,
-//    val user: User,
-//    val Auth: Auth
->>>>>>>> origin/main:app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/data/model/Product.kt
 )
