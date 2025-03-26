f97b6d9a3165d7a4ef668e675cbd3e5c5d3819f4
Noah Ra <rathaj9@gmail.com>
2025-03-26 16:10:26 +0100

    removed junk

--- app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/data/model/Geolocation.kt
+++
@@ -1,7 +0,0 @@
-package de.syntax_institut.jetpack.a04_05_online_shopper.data.model
-
-data class Geolocation(
-    val lat: String,
-    val long: String,
-)
-
--- app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/data/model/Name.kt
+++
@@ -1,7 +0,0 @@
-package de.syntax_institut.jetpack.a04_05_online_shopper.data.model
-
-data class Name(
-    val firstname: String,
-    val lastname: String,
-)
-
